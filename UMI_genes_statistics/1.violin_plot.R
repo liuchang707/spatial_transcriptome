618e9a778291ec44c50fe5da505e3d7a34443dec
liuchang707 <49985842+liuchang707@users.noreply.github.com>
2023-09-13 09:24:12 +0800

    Update 1.violin_plot.R

--- UMI_genes_statistics/1.violin_plot.R
+++ UMI_genes_statistics/1.violin_plot.R
@@ -5,6 +5,7 @@ library('ggplot2')
 
 pdf("nGene per Pixel.pdf")
 aa <- read.table('gene_count_wanted',header=T,sep = '\t')
+#### y will be scaled to logy !!!!!!
 ggplot(aa,aes(x=sample,y=genes,fill=sample))+geom_violin()+scale_y_log10()+ylab("nGene per Pixel")+
   theme_bw()+ theme(panel.background = element_rect(fill = 'white', color = 'black'))
 
@@ -21,21 +22,21 @@ ggplot(aa,aes(x=density,y=UMIs,fill=density))+geom_violin()+scale_y_log10()+ yla
   theme_bw()+ theme(panel.background = element_rect(fill = 'white', color = 'black'))
 dev.off()
 
-pdf("nGene per ¦Ìm2.pdf")
+pdf("nGene per Â¦ÃŒm2.pdf")
 aa <- read.table('30pixel2_gene_count_wanted',header=T,sep = '\t')
-ggplot(aa,aes(x=sample,y=genes,fill=sample))+geom_violin()+scale_y_log10()+ylab("nGene per ¦Ìm2")+
+ggplot(aa,aes(x=sample,y=genes,fill=sample))+geom_violin()+scale_y_log10()+ylab("nGene per Â¦ÃŒm2")+
   theme_bw()+ theme(panel.background = element_rect(fill = 'white', color = 'black'))
 dev.off()
 
-pdf("nUMI per ¦Ìm2.pdf") 
+pdf("nUMI per Â¦ÃŒm2.pdf") 
 aa <- read.table('30pixel2_UMI_count_wanted',header=T,sep = '\t')
-ggplot(aa,aes(x=sample,y=UMIs,fill=sample))+geom_violin()+scale_y_log10()+ylab("nUMI per ¦Ìm2")+
+ggplot(aa,aes(x=sample,y=UMIs,fill=sample))+geom_violin()+scale_y_log10()+ylab("nUMI per Â¦ÃŒm2")+
   theme_bw()+ theme(panel.background = element_rect(fill = 'white', color = 'black'))
 dev.off()
 
-pdf("pixel density per ¦Ìm2.pdf")
+pdf("pixel density per Â¦ÃŒm2.pdf")
 aa <- read.table('30pixel2_pixel_density',header=T,sep = '\t')
-ggplot(aa,aes(x=sample,y=counts,fill=sample))+geom_violin()+scale_y_log10()+ylab(expression(paste("nUMI per ",¦Ì, m^2)))+
+ggplot(aa,aes(x=sample,y=counts,fill=sample))+geom_violin()+scale_y_log10()+ylab(expression(paste("nUMI per ",Â¦ÃŒ, m^2)))+
   theme_bw()+ theme(panel.background = element_rect(fill = 'white', color = 'black'))
 dev.off()
 
